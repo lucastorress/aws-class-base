9f85d05a378b5d78533a9887bd5155811403b0c5
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2021-03-07 19:53:04 -0300

    Update data

--- user-data.sh
+++ user-data.sh
@@ -14,7 +14,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.nvm/nvm.sh \
 . /home/ubuntu/.bashrc
 
 nvm alias default v14.16.0
@@ -22,12 +22,12 @@ nvm install v14.16.0
 nvm use v14.16.0
 npm install pm2@latest -g
 
-sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/bin/node" \
-sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node" \
-sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/bin/npm" \
-sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm" \
-sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/bin/pm2" \
-sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/local/bin/pm2"
+ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/bin/node" \
+ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node" \
+ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/bin/npm" \
+ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm" \
+ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/bin/pm2" \
+ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/local/bin/pm2"
 
 env PATH=$PATH:/.nvm/versions/node/v14.16.0/bin /.nvm/versions/node/v14.16.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 env PATH=$PATH:/.nvm/versions/node/v14.16.0/bin pm2 startup systemd -u ubuntu --hp /home/ubuntu
